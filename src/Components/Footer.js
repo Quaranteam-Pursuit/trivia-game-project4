8cb41163889544bd460df07b5decd5b5388cda00
J Brodie Day <jbrodieday@gmail.com>
2021-10-13 18:31:54 -0400

    Tweak for netlify deploy

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return(
-    <p>created at <a href="https://junocollege.com/">juno college</a></p>
+    <p>Created using the <a href="https://opentdb.com/">Open Trivia Database API</a> at <a href="https://junocollege.com/">Juno College</a></p>
   )
 }
 
